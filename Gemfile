f86a7a9fa542eb2ab0584960c918d2c8f8235d66
schneems <richard.schneeman@gmail.com>
2012-02-22 14:38:38 -0800

    devise import works

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'devise',                  "~> 2.0.4"
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WickedExample::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +57,6 @@ WickedExample::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  root :to => "pages#index"
 end
