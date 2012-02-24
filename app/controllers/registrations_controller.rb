bd3ab21f08e46c1f4118dbb2de8ef530499feca8
schneems <richard.schneeman@gmail.com>
2012-02-24 11:05:22 -0800

    remove comment

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
     def after_sign_up_path_for(resource)
-      # previous_path_or(resource)
       after_register_path(:add_zip)
     end
 end
