9ac37c6deab19a343f71354187423eb7ad46c44c
schneems <richard.schneeman@gmail.com>
2012-03-13 16:42:06 -0700

    add cache_control and set to serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,7 @@ Example::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -19,6 +18,8 @@ Example::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.static_cache_control = "public, max-age=2592000"
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
